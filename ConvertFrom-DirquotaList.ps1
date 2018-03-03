bc7fbefe417f4fe2d819d3a5d3ea4b4d6ea03df8
Adam Mnich <adammnich@gmail.com>
2018-03-03 20:26:34 +0100

    Update ConvertFrom-DirquotaList.ps1

--- ConvertFrom-DirquotaList.ps1
+++ ConvertFrom-DirquotaList.ps1
@@ -1,7 +1,7 @@
 function ConvertFrom-DirquotaList {
     <#
 	.SYNOPSIS
-	Convert output from Dirquota quota list to an object 
+	Convert output from Dirquota quota list to an object
 	.NOTES
 	Original code from Technet gallery by Ben Wilkinson https://gallery.technet.microsoft.com/scriptcenter/Find-and-Report-on-cc49120e
 #>
@@ -11,11 +11,12 @@ function ConvertFrom-DirquotaList {
         $InputObject
     )
     BEGIN {
-    	#region Helper functions
+
+        #region Helper functions
         function Convert-Size {
-			<#
+            <#
 			.SYNOPSIS
-			helper function to convert size to GB			
+			helper function to convert size to GB
 			#>
             param (
                 $string, $seperator, $Figure
@@ -38,6 +39,7 @@ function ConvertFrom-DirquotaList {
             }
         }
         #endregion
+
         $pattern = '^.{24}'
         $dirQuotaItems = @()
     }
@@ -46,14 +48,14 @@ function ConvertFrom-DirquotaList {
     }
     END {
         if ($dirQuotaItems.count -lt 4) {
-            throw $dirQuotaItems		
+            throw $dirQuotaItems
         }
         $dirQuotaItems = $dirQuotaItems -replace '˙'
-	    
+
         for ($i = 0; $i -lt $dirQuotaItems.count - 1; $i++) {
-            if (($dirQuotaItems[$i] -match "Quota Path:") -or ($dirQuotaItems[$i] -match "Kontingentpfad:")) { 
-                [String]$QuotaPath = ($dirQuotaItems[$i] -split $pattern)[1] 
-                if (($dirQuotaItems[$i + 1] -match "Share Path:") -or ($dirQuotaItems[$i + 1] -match "Freigabepfad:")) { 
+            if (($dirQuotaItems[$i] -match "Quota Path:") -or ($dirQuotaItems[$i] -match "Kontingentpfad:") -or ($dirQuotaItems[$i] -match "ka przydzia.u:")) {
+                [String]$QuotaPath = ($dirQuotaItems[$i] -split $pattern)[1]
+                if (($dirQuotaItems[$i + 1] -match "Share Path:") -or ($dirQuotaItems[$i + 1] -match "Freigabepfad:")) {
                     [String]$SharePath = ($dirQuotaItems[$i + 1] -split $pattern)[1]
                 } else {
                     $SharePath = $null
@@ -61,11 +63,11 @@ function ConvertFrom-DirquotaList {
                 }
                 [String]$Template, $TemplateMatch = (($dirQuotaItems[$i + 2] -split $pattern)[1] -split "\(")
                 $templateMatch = $templateMatch.Substring(0, $templateMatch.Length - 1)
-                [String]$QuotaStatus = ($dirQuotaItems[$i + 3] -split $pattern)[1] 
+                [String]$QuotaStatus = ($dirQuotaItems[$i + 3] -split $pattern)[1]
                 [String]$LimitString, $LimitFigure, $LimitType = (($dirQuotaItems[$i + 4] -split $pattern)[1] -split " ")
                 if (!$seperator) {
                     if ($LimitString -match '\..*\,') {
-                        $seperator = ','                        
+                        $seperator = ','
                     } elseif ($LimitString -match '\.') {
                         $seperator = '.'
                     } else {
@@ -73,39 +75,39 @@ function ConvertFrom-DirquotaList {
                     }
                 }
                 $limit = Convert-Size -Figure $LimitFigure -seperator $seperator -string $LimitString
-                [String]$UsedString, $UsedFigure, $PercentageUsed = (($dirQuotaItems[$i + 5] -split $pattern)[1] -split " ") 
+                [String]$UsedString, $UsedFigure, $PercentageUsed = (($dirQuotaItems[$i + 5] -split $pattern)[1] -split " ")
                 $Used = Convert-Size -string $UsedString -figure $UsedFigure -seperator $seperator
-                [String]$AvailableString, $AvailableFigure = (($dirQuotaItems[$i + 6] -split $pattern)[1] -split " ") 
+                [String]$AvailableString, $AvailableFigure = (($dirQuotaItems[$i + 6] -split $pattern)[1] -split " ")
                 $Available = Convert-Size -seperator $seperator -figure $AvailableFigure -string $AvailableString
                 [String]$PeakString, $PeakFigure, $PeakDate1, $PeakDate2 = (($dirQuotaItems[$i + 7] -split $pattern)[1] -split " ")
                 $peak = Convert-Size -seperator $seperator -figure $PeakFigure -string $PeakString
                 $PeakDate1 = $PeakDate1 -replace "\("
                 $PeakDate2 = $PeakDate2 -replace "\)"
                 $i = $i + 7
-                [Int]$UsedPercentage = (($PercentageUsed -split "%")[0]).Substring(1) 
-                if ($UsedPercentage -gt 100) { 
-                    $AvailablePC = 0 
-                } else { 
-                    $AvailablePC = (100 - $UsedPercentage) 
-                }   
-                $Hash = @{ 
-                    QuotaPath     = $QuotaPath 
+                [Int]$UsedPercentage = (($PercentageUsed -split "%")[0]).Substring(1)
+                if ($UsedPercentage -gt 100) {
+                    $AvailablePC = 0
+                } else {
+                    $AvailablePC = (100 - $UsedPercentage)
+                }
+                $Hash = @{
+                    QuotaPath     = $QuotaPath
                     SharePath     = $SharePath
-                    Status        = $QuotaStatus 
-                    LimitGB       = $Limit 
-                    LimitType     = ($LimitType).Substring(1, $LimitType.length - 2) 
-                    UsedGB        = $Used 
-                    UsedPC        = $UsedPercentage 
-                    AvailablePC   = $AvailablePC 
-                    AvailableGB   = $Available 
+                    Status        = $QuotaStatus
+                    LimitGB       = $Limit
+                    LimitType     = ($LimitType).Substring(1, $LimitType.length - 2)
+                    UsedGB        = $Used
+                    UsedPC        = $UsedPercentage
+                    AvailablePC   = $AvailablePC
+                    AvailableGB   = $Available
                     PeakGB        = $Peak
                     PeakDate      = "$PeakDate1 $PeakDate2"
                     Template      = $Template
                     TemplateMatch = $templateMatch
-                } 
-                New-Object PSObject -Property $Hash | 
-                    Select-Object -Property QuotaPath, SharePath, LimitGB, UsedGB, UsedPC, AvailableGB, AvailablePC, PeakGB, PeakDate, Template, TemplateMatch, LimitType, Status 
+                }
+                New-Object PSObject -Property $Hash |
+                    Select-Object -Property QuotaPath, SharePath, LimitGB, UsedGB, UsedPC, AvailableGB, AvailablePC, PeakGB, PeakDate, Template, TemplateMatch, LimitType, Status
             }
-        } 
+        }
     }
 }
